9056919931c8e059540a60fe1f4c8b4e4a95219a
AEnjoy <964728598@qq.com>
2020-04-30 00:00:16 +0800

    Root Tool Box

---
+++ adbshellpy_libmagiskmm.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#   adbshellpy_libmagiskmm
+#       By : 神郭
+#  Version : 1.0
+import sys,os,zipfile
+try:
+    from adbshell import errexit
+    from adbshell import update
+    from adbshell import checkinternet
+    from adbshell import clear
+    from adbshell import adbcommand
+    from adbshell import install
+    from adbshell import changes
+    from adbshell import github
+    from adbshell import version
+    from adbshell import builddate
+    from adbshell import p
+    from adbshell import adbfile
+    from adbshell import conf   
+except:
+    from adbshell_alpha import errexit
+    from adbshell_alpha import update
+    from adbshell_alpha import checkinternet
+    from adbshell_alpha import clear
+    from adbshell_alpha import adbcommand
+    from adbshell_alpha import install
+    from adbshell_alpha import changes
+    from adbshell_alpha import github
+    from adbshell_alpha import version
+    from adbshell_alpha import builddate
+    from adbshell_alpha import p
+    from adbshell_alpha import adbfile
+    from adbshell_alpha import conf
+class adbshellpyinformation:
+    branch=None
+    uselinuxpkgmanagertoinstalladb=None
+    adbfile=None
+    aapt=None
